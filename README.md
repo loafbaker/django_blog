1b29d70569a3b75e64c54017f59a59fcd0c26697
Jianming Chen <loafbaker@hotmail.com>
2016-03-04 22:21:17 -0600

    Add customized admin

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ A tutorial basic blog web-app based on the framework of Django 1.9.
 Run successfully within Django 1.9.3 and Python 2.7.6
 
 Ver.1   [Install & Admin](../../tree/63967d94c2443d3665a435d4b1ad3448e8f59131)
+
+Ver.2   [First App & Model](../../tree/1a0ff02f17824fe3b1a8435e18138b046669638f)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['title', 'updated', 'timestamp']
+	list_editable = ['title']
+	list_display_links = ['updated']
+	list_filter = ['timestamp', 'updated']
+	search_fields = ['title', 'content']
+	class Meta:
+		model = Post
+
+admin.site.register(Post, PostAdmin)
